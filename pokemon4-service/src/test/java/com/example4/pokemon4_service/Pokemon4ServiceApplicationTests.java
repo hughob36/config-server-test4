b6fd08432fb50d9a28f0fb372647d11320776eeb
Hugo Benitez <hugo.boca16@gmail.com>
2026-03-02 18:31:09 -0300

    pokemon service

---
+++ pokemon4-service/src/test/java/com/example4/pokemon4_service/Pokemon4ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example4.pokemon4_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pokemon4ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
